72378f6b105846425b252ba56aa1cf224a9a2e17
jintian.wang <jintiaawang@tencent.com>
2025-06-28 17:58:04 +0800

    feat: init

---
+++ docker_engine_install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+os='unknown'
+
+os=check_os
+echo "Detected OS: $os"
+
+
+
+
+check_os() {
+    if [[ -f /etc/redhat-release ]]; then
+        os="centos"
+    elif cat /etc/issue | grep -Eqi "debian"; then
+        os="debian"
+    elif cat /etc/issue | grep -Eqi "ubuntu"; then
+        os="ubuntu"
+    elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+        os="centos"
+    elif cat /proc/version | grep -Eqi "debian"; then
+        os="debian"
+    elif cat /proc/version | grep -Eqi "ubuntu"; then
+        os="ubuntu"
+    elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+        os="centos"
+    fi
+}
